a68027147921b2f9cd0fb124d2aefb98431e4319
kakimoto <sato.kakimoto@gmail.com>
2023-05-03 11:53:51 +0900

    add config

---
+++ config.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+
+global xxx
--- var2gl.py
+++ var2gl.py
@@ -6,24 +6,7 @@ from lib import load_yaml
 local_path = ''
 
 
-def _bet():
+def _xxx():
 
     path = load_yaml._load_yaml(local_path + 'path.yml')
-    config.path_bet = path['bet']
-    config.path_model = path['model']
-    config.path_races = path['races']
-    config.path_result = path['result']
-    config.path_setting = path['setting']
-    config.path_statistics = path['statistics']
-
-
-def _investment():
-
-    path = load_yaml._load_yaml(local_path + 'path.yml')
-    config.path_investment = path['investment']
-
-
-def _transscript():
-
-    path = load_yaml._load_yaml(local_path + 'path.yml')
-    config.path_transscript = path['transscript']
+    config.xxx = path['xxx']
